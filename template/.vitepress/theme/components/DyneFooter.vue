ea4deb4b68974c96d752671d8db0cecac3f25482
Jaromil <jaromil@dyne.org>
2026-05-29 23:23:10 +0200

    feat: add dyne footer

---
+++ template/.vitepress/theme/components/DyneFooter.vue
@@ -0,0 +1,63 @@
+<template>
+  <footer class="dyne-footer">
+    <div class="dyne-footer__inner">
+      <div class="dyne-footer__content">
+        <div class="dyne-footer__top">
+          <a class="dyne-footer__logo" href="https://dyne.org" aria-label="Dyne.org">
+            <img class="dyne-footer__mark" src="/dyne-mark.svg" alt="" />
+            <img class="dyne-footer__logotype" src="/dyne-logotype.svg" alt="Dyne.org" />
+          </a>
+
+          <nav class="dyne-footer__socials" aria-label="Dyne.org social links">
+            <a
+              v-for="social in socialLinks"
+              :key="social.name"
+              class="dyne-footer__social"
+              :href="social.href"
+              :aria-label="`Follow Dyne.org on ${social.name}`"
+              target="_blank"
+              rel="me noopener"
+            >
+              {{ social.shortName }}
+            </a>
+          </nav>
+        </div>
+
+        <div class="dyne-footer__legal">
+          <p>
+            Copyleft &copy;1999-{{ currentYear }} Dyne.org foundation<br />
+            Haparandadam 7-A1 1013AK Amsterdam
+          </p>
+          <p>
+            Not for profit org. est. 2005 with reg.num. 34237525 at KvK.NL<br />
+            <a href="https://dyne.org/donate">Make a donation.</a>
+          </p>
+          <p>
+            The content of this website are licensed under a
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+              Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+
+const socialLinks = [
+  { name: 'twitter', shortName: 'Tw', href: 'https://socials.dyne.org/twitter' },
+  { name: 'telegram', shortName: 'Tg', href: 'https://socials.dyne.org/telegram' },
+  { name: 'github', shortName: 'Gh', href: 'https://socials.dyne.org/github' },
+  { name: 'linkedin', shortName: 'In', href: 'https://socials.dyne.org/linkedin' },
+  { name: 'instagram', shortName: 'Ig', href: 'https://socials.dyne.org/instagram' },
+  { name: 'facebook', shortName: 'Fb', href: 'https://socials.dyne.org/facebook' },
+  { name: 'youtube', shortName: 'Yt', href: 'https://socials.dyne.org/youtube' },
+  { name: 'mastodon', shortName: 'Ma', href: 'https://socials.dyne.org/mastodon' },
+  { name: 'medium', shortName: 'Me', href: 'https://socials.dyne.org/medium' },
+  { name: 'discord', shortName: 'Di', href: 'https://socials.dyne.org/discord' },
+  { name: 'matrix', shortName: 'Mx', href: 'https://socials.dyne.org/matrix' }
+]
+</script>
--- template/.vitepress/theme/index.ts
+++ template/.vitepress/theme/index.ts
@@ -2,14 +2,15 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import DyneBrand from './components/DyneBrand.vue'
+import DyneFooter from './components/DyneFooter.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      'nav-bar-title-before': () => h(DyneBrand, { variant: 'nav' })
+      'nav-bar-title-before': () => h(DyneBrand, { variant: 'nav' }),
+      'layout-bottom': () => h(DyneFooter)
     })
   }
 } satisfies Theme
-
